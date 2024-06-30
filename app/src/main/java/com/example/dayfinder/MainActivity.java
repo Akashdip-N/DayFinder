eb161e8f45057d3af2510a6826452fe79e5035b3
Akashdip Neogi <Akashdipneogi@gmail.com>
2024-06-30 21:56:59 +0100

    optimized the code and it's spacings

--- app/src/main/java/com/example/dayfinder/MainActivity.java
+++ app/src/main/java/com/example/dayfinder/MainActivity.java
@@ -1,22 +1,18 @@
 package com.example.dayfinder;
 
 import static android.view.View.GONE;
-import static android.view.View.OnClickListener;
 import static android.view.View.VISIBLE;
 import static android.widget.Toast.LENGTH_SHORT;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity
 {
