7610e232f47e59fef505a503d5d795da3ccb3129
Akashdip Neogi <Akashdipneogi@gmail.com>
2024-06-30 08:41:53 +0100

    Added SuppressLint part in the code

--- app/src/main/java/com/example/dayfinder/MainActivity.java
+++ app/src/main/java/com/example/dayfinder/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity
     EditText Input;
     TextView copyright, result, instructions;
     Button tryagain, contact, rate, submit ;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
