44c0860a54f8b853c9be18d3d03c5f6466f2ec0a
Akashdip Neogi <Akashdipneogi@gmail.com>
2024-08-15 11:43:09 +0100

    Removed Unused import files

--- app/src/main/java/com/example/dayfinder/MainActivity.java
+++ app/src/main/java/com/example/dayfinder/MainActivity.java
@@ -8,15 +8,12 @@ import static android.widget.Toast.LENGTH_SHORT;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity
 {
